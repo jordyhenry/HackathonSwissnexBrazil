4c846593bbc4bdd1c5c0bdf32baeb0599c4b50de
jordyhenry <jordy.hpereira@gmail.com>
2019-08-18 12:09:55 -0300

    serial port changer

---
+++ Assets/HackatonSwissnex/Script/SerialPortChanger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SerialPortChanger : MonoBehaviour
+{
+    public SerialController serialController;
+    public Text consoleText;
+    public InputField portInput;
+    public GameObject canvasContainer;
+
+    private void Start()
+    {
+        canvasContainer.SetActive(false);
+    }
+
+    public void ChangePort()
+    {
+        serialController.gameObject.SetActive(false);
+        serialController.portName = portInput.text;
+        serialController.gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            canvasContainer.SetActive(!canvasContainer.activeSelf);
+        }
+    }
+}
